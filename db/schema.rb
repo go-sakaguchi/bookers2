fa985bfa17315c2d512dbb8778d92bd27ccdad69
go-sakaguchi <123789515+go-sakaguchi@users.noreply.github.com>
2023-06-07 02:55:39 +0000

    [Fix]userpage_rightside

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_06_034400) do
+ActiveRecord::Schema.define(version: 2023_06_07_022151) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 2023_06_06_034400) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
-    t.integer "user-id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
